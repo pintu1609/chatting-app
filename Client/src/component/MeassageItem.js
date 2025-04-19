03f10ceae0578fb8e4b6f64b9d1446ce425a8c18
Pintu <pintu@Mac.lan>
2025-04-19 23:50:42 +0530

    improved meassge responsive

--- Client/src/component/MeassageItem.js
+++ Client/src/component/MeassageItem.js
@@ -1,19 +1,24 @@
 import React from "react";
 
 const MeassageItem = (props) => {
+  const isUser = props.color;
+
   return (
-    <div className="meassageitem owner">
+    <div className={`meassageitem ${isUser ? "owner" : "receiver"}`}>
       <div className="meassageInfo">
-        <img src={props.userImages} alt="" />
+        <img src={props.userImages} alt="User" />
         <span>{props.timeDifference}</span>
       </div>
 
-      <div
-        className="meassagecontent"
-      >
-        <p style={{ backgroundColor: `${props.color ? "" : "white"}`, color:`${props.color ? "":"#5d5d8d" }`}}> 
-        {props.message}</p>
-        {/* <img src='Images/pintu.jpeg' alt=''/> */}
+      <div className="meassagecontent">
+        <p
+          style={{
+            backgroundColor: isUser ? "#5d5d8d" : "white",
+            color: isUser ? "white" : "#5d5d8d",
+          }}
+        >
+          {props.message}
+        </p>
       </div>
     </div>
   );
